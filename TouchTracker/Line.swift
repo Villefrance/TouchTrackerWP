1a1d43c5530ae96fbeddaa27840e8aab57997c33
Jacob Nielsen <jani@mmmi.sdu.dk>
2017-10-24 11:34:49 +0200

    Single Touch

---
+++ TouchTracker/Line.swift
@@ -0,0 +1,15 @@
+//
+//  Line.swift
+//  TouchTracker
+//
+//  Created by Jacob Nielsen on 24/10/2017.
+//  Copyright © 2017 Jacob Nielsen. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+struct Line {
+    var begin = CGPoint.zero
+    var end = CGPoint.zero
+}
